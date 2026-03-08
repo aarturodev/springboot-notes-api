2fb10dc7cc3213b02074e1da39488346ff8642a1
Anderson Marin <andersonarturomarin@gmail.com>
2026-03-07 22:07:14 -0500

    refactor: add creation and update timestamps

--- src/main/java/com/anderson/notes_api/model/Note.java
+++ src/main/java/com/anderson/notes_api/model/Note.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 import java.time.LocalDateTime;
@@ -24,6 +26,10 @@ public class Note {
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @CreationTimestamp()
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp()
     private LocalDateTime updatedAt;
 }
