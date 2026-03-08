936c6c0e06ce3dbad51e808fa34dfdffb2e368e6
Anderson Marin <andersonarturomarin@gmail.com>
2026-03-07 20:31:34 -0500

    add Note entity

---
+++ src/main/java/com/anderson/springboot_notes_api/model/Note.java
@@ -0,0 +1,29 @@
+package com.anderson.springboot_notes_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
