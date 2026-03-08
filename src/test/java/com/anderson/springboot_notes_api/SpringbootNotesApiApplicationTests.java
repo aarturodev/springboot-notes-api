6dfa74dcc31a59e297a15cf4ca9a6f346bddc010
Anderson Marin <andersonarturomarin@gmail.com>
2026-03-07 20:19:44 -0500

    Initial Spring Boot project

---
+++ src/test/java/com/anderson/springboot_notes_api/SpringbootNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.springboot_notes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
